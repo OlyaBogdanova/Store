06bba02d10d3971be6987eaf605d918dedd31213
OlgaB <olyavasilenko1992@gmail.com>
2022-10-18 10:31:51 +0300

    fdf

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,7 +6,7 @@ const Navigation = () => {
 <nav className='h-[50px] flex justify-between px-5 items-center bg-gray-500 text-white'>
 <span className='font-bold'>React 2022</span>
 <span>
-    <Link to='/' className='mr-2'>Products</Link>
+    <Link to='/Store/' className='mr-2'>Products</Link>
     <Link to='/about'>About</Link>
 </span>
 
