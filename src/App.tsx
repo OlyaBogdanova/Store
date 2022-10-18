0c2aa2ad8eb983cd644c974ee3a643c5e6dea7c1
OlgaB <olyavasilenko1992@gmail.com>
2022-10-18 10:18:13 +0300

    third

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
             <Navigation />
             <Routes>
                 <Route
-                    path='/'
+                    path='/Store/'
                     element={<ProductsPage />}
                 />
                 <Route
